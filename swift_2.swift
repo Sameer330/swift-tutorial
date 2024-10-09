1b2c6453b8a2806f796fe1f7ba5c105bc2b5b750
Sameer Ankalagi <330.sameer@gmail.com>
2024-10-09 17:49:56 +0530

    operators, user input and typecasting

---
+++ swift_2.swift
@@ -0,0 +1,27 @@
+// OPERATORS
+
+/* 
+1. Assignment Operators [=]
+2. Arithmetic Operators [+ - * /]
+3. Remainder Operator
+4. Unary Minus Operator
+5. Unary Plus Operator
+6. Compound Assignment Operators
+7. Comparison Operators
+8. Ternary Conditional Operator
+9. Range Operators
+10. Logical Operators [&& || !]
+11. Relational Operators [== != > < >= <=]
+
+Swift does not have an increment(++) or decrement(--) operator.
+*/
+
+// USER INPUT
+print("Enter a value: ");
+var v1 = readLine()!
+
+print("The value is \(v1!)"); // String value
+
+// TYPE CASTING
+var v1: Int = Int(v1)!
+print("The value is \(v1)"); // Integer value
