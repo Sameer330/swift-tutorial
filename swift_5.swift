a5bb92009a88c6082421d9ad63543dedfc399c87
Sameer Ankalagi <330.sameer@gmail.com>
2024-10-11 19:08:12 +0530

    conditionals, while loop and for loop

---
+++ swift_5.swift
@@ -0,0 +1,35 @@
+// CONDITIONAL STATEMENTS
+
+print("Enter age: ");
+var age: Int = 0;
+age = Int(readLine()!)!;
+
+if(age >= 18) {
+    print("You are eligible to vote.");
+} else {
+    print("You are not eligible to vote.");
+}
+
+// WHILE LOOP
+print("Enter a number: ");
+var n: Int = 0;
+n = Int(readLine()!)!;
+
+var i: Int = 0;
+while(i < n) {
+    print(i);
+    i += 1;
+}
+
+// FOR LOOP
+print("For Loop");
+for i in 0...n {
+    print(i);
+}
+
+var sum: Int = 0;
+for i in 0...n {
+    print("Count", i);
+    sum = sum + i;
+}
+print(sum);
